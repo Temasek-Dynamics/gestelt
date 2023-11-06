4ccce48dd781ca4aba3f335af1841e42de91fc59
johntgz <john_tanguanzhong@hotmail.com>
2023-11-06 11:11:57 +0800

    updated launch files for demo deployment

--- gestelt_bringup/scripts/vicon/vicon_central.sh
+++ gestelt_bringup/scripts/vicon/vicon_central.sh
@@ -25,22 +25,13 @@ export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$gestelt_bringup_DIR:$PX4_AUTOPILOT_RE
 # Commands
 #####
 
-# CMD_0="
-# roslaunch gestelt_bringup vicon_drone1_sim.launch world_name:=$SCRIPT_DIR/../simulation/worlds/ego_test.world
-# "
+CMD_0="roslaunch gestelt_bringup demo_fake_drone.launch"
 
-CMD_1="roslaunch gestelt_bringup demo_fake_drone.launch"
+CMD_1="roslaunch gestelt_bringup vicon_client.launch"
 
+CMD_2="roslaunch gestelt_bringup vicon_central.launch rviz_config:=gz_sim"
 
-CMD_2="
-roslaunch gestelt_bringup vicon_client.launch 
-"
-
-CMD_3="
-roslaunch gestelt_bringup vicon_central.launch rviz_config:=gz_sim
-"
-
-CMD_4="roslaunch gestelt_bringup vicon_multi_misson.launch"
+CMD_3="roslaunch gestelt_bringup vicon_multi_misson.launch"
 
 if [ "$SESSIONEXISTS" = "" ]
 then 
@@ -54,11 +45,11 @@ then
 
     # tmux send-keys -t $SESSION:0.0 "$SOURCE_PX4_AUTOPILOT $CMD_0" C-m 
     sleep 1
-    tmux send-keys -t $SESSION:0.1 "$SOURCE_WS $CMD_1" C-m 
+    tmux send-keys -t $SESSION:0.0 "$SOURCE_WS $CMD_0" C-m 
     sleep 1
-    tmux send-keys -t $SESSION:0.2 "$SOURCE_WS $CMD_2" C-m 
-    tmux send-keys -t $SESSION:0.3 "$SOURCE_WS $CMD_3" C-m
-    tmux send-keys -t $SESSION:0.4 "$SOURCE_WS $CMD_4" 
+    tmux send-keys -t $SESSION:0.1 "$SOURCE_WS $CMD_1" C-m 
+    tmux send-keys -t $SESSION:0.2 "$SOURCE_WS $CMD_2" C-m
+    tmux send-keys -t $SESSION:0.3 "$SOURCE_WS $CMD_3" 
 
 fi
 
