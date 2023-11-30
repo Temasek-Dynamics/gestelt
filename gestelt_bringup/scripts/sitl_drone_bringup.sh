c4bcbe286e040c4ca058de96ea77795ad12d2c00
tianchensun <tianchen_sun787@163.com>
2023-11-30 19:58:56 +0800

    mod for PX4 v1.13.0

--- gestelt_bringup/scripts/sitl_drone_bringup.sh
+++ gestelt_bringup/scripts/sitl_drone_bringup.sh
@@ -16,11 +16,17 @@ PX4_AUTOPILOT_REPO_DIR="$SCRIPT_DIR/../../../PX4-Autopilot"
 SOURCE_WS="
 source $SCRIPT_DIR/../../../devel/setup.bash &&
 "
-SOURCE_PX4_AUTOPILOT="
-source $PX4_AUTOPILOT_REPO_DIR/Tools/simulation/gazebo-classic/setup_gazebo.bash $PX4_AUTOPILOT_REPO_DIR $PX4_AUTOPILOT_REPO_DIR/build/px4_sitl_default &&
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$gestelt_bringup_DIR:$PX4_AUTOPILOT_REPO_DIR:$PX4_AUTOPILOT_REPO_DIR/Tools/simulation/gazebo-classic/sitl_gazebo-classic &&
-"
+# PX4 v1.14.0
+# SOURCE_PX4_AUTOPILOT="
+# source $PX4_AUTOPILOT_REPO_DIR/Tools/simulation/gazebo-classic/setup_gazebo.bash $PX4_AUTOPILOT_REPO_DIR $PX4_AUTOPILOT_REPO_DIR/build/px4_sitl_default &&
+# export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$gestelt_bringup_DIR:$PX4_AUTOPILOT_REPO_DIR:$PX4_AUTOPILOT_REPO_DIR/Tools/simulation/gazebo-classic/sitl_gazebo-classic &&
+# "
 
+# PX4 v1.13.0
+SOURCE_PX4_AUTOPILOT="
+source $PX4_AUTOPILOT_REPO_DIR/Tools/setup_gazebo.bash $PX4_AUTOPILOT_REPO_DIR $PX4_AUTOPILOT_REPO_DIR/build/px4_sitl_default &&
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$gestelt_bringup_DIR:$PX4_AUTOPILOT_REPO_DIR:$PX4_AUTOPILOT_REPO_DIR/Tools/sitl_gazebo &&
+"
 #####
 # Commands
 #####
@@ -42,6 +48,9 @@ roslaunch trajectory_planner trajectory_planner_node.launch
 # Start up script to send commands
 CMD_3="roslaunch gestelt_bringup mission.launch"
 
+# disarm drone
+# CMD_4="rosservice call /drone_commander/disarm"
+# CMD_4="rosrun mavros mavparam set COM_RCL_EXCEPT 4"
 if [ "$SESSIONEXISTS" = "" ]
 then 
 
@@ -53,11 +62,11 @@ then
 
     tmux send-keys -t $SESSION:0.0 "$SOURCE_PX4_AUTOPILOT $CMD_0" C-m 
     sleep 2
-    tmux send-keys -t $SESSION:0.1 "$SOURCE_WS $CMD_1" C-m 
+    tmux send-keys -t $SESSION:0.1 "$SOURCE_WS $CMD_1 &" C-m 
     sleep 1
     tmux send-keys -t $SESSION:0.2 "$SOURCE_WS $CMD_2" C-m 
     sleep 1
-    tmux send-keys -t $SESSION:0.3 "$SOURCE_WS $CMD_3" C-m
+    tmux send-keys -t $SESSION:0.3 "$SOURCE_WS $CMD_3" #C-m
 fi
 
 # Attach session on the first window
