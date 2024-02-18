97a36825a45f443a5a8275aa06e352d4c9b98366
tianchensun <tianchen_sun787@163.com>
2024-02-18 09:53:26 +0800

    refine sh

--- gestelt_bringup/scripts/sitl_drone_bringup_learning_agile_drone_distributed.sh
+++ gestelt_bringup/scripts/sitl_drone_bringup_learning_agile_drone_distributed.sh
@@ -28,6 +28,12 @@ SOURCE_PX4_AUTOPILOT="
 source $PX4_AUTOPILOT_REPO_DIR/Tools/setup_gazebo.bash $PX4_AUTOPILOT_REPO_DIR $PX4_AUTOPILOT_REPO_DIR/build/px4_sitl_default &&
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$gestelt_bringup_DIR:$PX4_AUTOPILOT_REPO_DIR:$PX4_AUTOPILOT_REPO_DIR/Tools/sitl_gazebo &&
 "
+
+# let the cpu run in the highest performance
+CPU_PERFORMANCE="
+cpufreq-set -g performance
+"
+
 #####
 # Commands
 #####
@@ -63,7 +69,7 @@ then
 
     tmux send-keys -t $SESSION:0.0 "$SOURCE_PX4_AUTOPILOT $CMD_0" #C-m 
     sleep 2
-    tmux send-keys -t $SESSION:0.1 "$SOURCE_WS $EXPORT_ROS_MASTER_URI $CMD_1" C-m 
+    tmux send-keys -t $SESSION:0.1 "$CPU_PERFORMANCE $SOURCE_WS $EXPORT_ROS_MASTER_URI $CMD_1" C-m 
     sleep 1
     tmux send-keys -t $SESSION:0.2 "$SOURCE_WS " #C-m 
     sleep 1
