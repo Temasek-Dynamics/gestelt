00c5cbaab936f636e7a8634a26389a702f50aafe
tianchensun <tianchen_sun787@163.com>
2024-07-15 13:11:42 +0800

    refine safety logic

--- learning_agile/src/learning_agile.cpp
+++ learning_agile/src/learning_agile.cpp
@@ -79,98 +79,100 @@ void LearningAgile::solver_request(){
         NO_SOLUTION_FLAG_=true;
         ROS_INFO("the request period is too long, emergency stop");
     }
-    
-    //t_tra: time to the traverse point relative to the current time
-    //t_tra_abs_: absolute time to the traverse point, w.r.t the mission start time
-    double mission_t_progress= std::chrono::duration_cast<std::chrono::duration<double>>(current_time - mission_start_time_).count();
-    double t_tra=t_tra_abs_-mission_t_progress;   
-    // ROS_INFO("t_tra is %f", t_tra);
-    
-    for (int i = 0; i < n_nodes_; i++)
+    else
     {
-        current_input_=last_input_;
-        double varying_trav_weight = max_tra_w_ * std::exp(-10 * std::pow(dt_ * i - t_tra, 2));
+        //t_tra: time to the traverse point relative to the current time
+        //t_tra_abs_: absolute time to the traverse point, w.r.t the mission start time
+        double mission_t_progress= std::chrono::duration_cast<std::chrono::duration<double>>(current_time - mission_start_time_).count();
+        double t_tra=t_tra_abs_-mission_t_progress;   
+        // ROS_INFO("t_tra is %f", t_tra);
+        
+        for (int i = 0; i < n_nodes_; i++)
+        {
+            current_input_=last_input_;
+            double varying_trav_weight = max_tra_w_ * std::exp(-10 * std::pow(dt_ * i - t_tra, 2));
 
-        // set the external parameters for the solver
-        // desired goal state, current input, desired traverse pose, varying traverse weight
+            // set the external parameters for the solver
+            // desired goal state, current input, desired traverse pose, varying traverse weight
+            Eigen::VectorXd solver_extern_param(22);
+            solver_extern_param.segment(0,10) = des_goal_state_;
+            solver_extern_param.segment(10,4) = current_input_;
+            solver_extern_param.segment(14,3) = des_trav_point_;
+            solver_extern_param.segment(17,4) = des_trav_quat_;
+            solver_extern_param(21) = varying_trav_weight;
+
+            
+            double *solver_extern_param_ptr = solver_extern_param.data();
+        
+            int NP=22;
+            ACADOS_model_acados_update_params(acados_ocp_capsule, i,solver_extern_param_ptr,NP);
+        }
+        //TODO
+        // set the initial GUESS
+
+        // set the end desired state
         Eigen::VectorXd solver_extern_param(22);
+        double end_weight=0;
         solver_extern_param.segment(0,10) = des_goal_state_;
         solver_extern_param.segment(10,4) = current_input_;
         solver_extern_param.segment(14,3) = des_trav_point_;
         solver_extern_param.segment(17,4) = des_trav_quat_;
-        solver_extern_param(21) = varying_trav_weight;
+        solver_extern_param(21) = end_weight;
 
         
         double *solver_extern_param_ptr = solver_extern_param.data();
-      
+    
         int NP=22;
-        ACADOS_model_acados_update_params(acados_ocp_capsule, i,solver_extern_param_ptr,NP);
-    }
-    //TODO
-    // set the initial GUESS
-
-    // set the end desired state
-    Eigen::VectorXd solver_extern_param(22);
-    double end_weight=0;
-    solver_extern_param.segment(0,10) = des_goal_state_;
-    solver_extern_param.segment(10,4) = current_input_;
-    solver_extern_param.segment(14,3) = des_trav_point_;
-    solver_extern_param.segment(17,4) = des_trav_quat_;
-    solver_extern_param(21) = end_weight;
-
-    
-    double *solver_extern_param_ptr = solver_extern_param.data();
-   
-    int NP=22;
-    ACADOS_model_acados_update_params(acados_ocp_capsule, n_nodes_, solver_extern_param_ptr, NP);
-    //set initial condition aligned with the current state
-    double *drone_state_ptr = drone_state_.data();
-    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "lbx",drone_state_ptr);
-    ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "ubx",drone_state_ptr);
+        ACADOS_model_acados_update_params(acados_ocp_capsule, n_nodes_, solver_extern_param_ptr, NP);
+        //set initial condition aligned with the current state
+        double *drone_state_ptr = drone_state_.data();
+        ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "lbx",drone_state_ptr);
+        ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "ubx",drone_state_ptr);
 
 
-    // solve the problem
-    status = ACADOS_model_acados_solve(acados_ocp_capsule);
-    if (status != 0){
-        NO_SOLUTION_FLAG_=true;
-        ROS_INFO("acados no solution");
-    }
-    else
-    {
-        // get the state solution for visualization
+        // solve the problem
+        status = ACADOS_model_acados_solve(acados_ocp_capsule);
+        if (status != 0){
+            NO_SOLUTION_FLAG_=true;
+            ROS_INFO("acados no solution");
+        }
+        else
+        {
+            // get the state solution for visualization
 
-        // if (PRED_TRAJ_VIS_FLAG_){
-            
-            
-            
-        //     for (int i = 0; i < n_nodes_; ++i)
-        //     {   
+            // if (PRED_TRAJ_VIS_FLAG_){
                 
-        //         ocp_nlp_get_at_stage(nlp_config, nlp_dims, nlp_out, i, "x", &state_i_opt_);
                 
-        //         ROS_INFO("state_i_opt_ is %f, %f, %f", state_i_opt_[0], state_i_opt_[1], state_i_opt_[2]);
-        //         // for (int j = 0; j < n_x_; ++j)
-        //         // {
-        //         //     state_traj_opt_.push_back(state_i_opt_);
-        //         // }
-    
-        //     }
-
-
-        // //     // // get the last state
-        // //     // ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, n_nodes_, "x", &state_i_opt_);
-            
-        // // pred_traj_vis();
-
-            
-        // }
+                
+            //     for (int i = 0; i < n_nodes_; ++i)
+            //     {   
+                    
+            //         ocp_nlp_get_at_stage(nlp_config, nlp_dims, nlp_out, i, "x", &state_i_opt_);
+                    
+            //         ROS_INFO("state_i_opt_ is %f, %f, %f", state_i_opt_[0], state_i_opt_[1], state_i_opt_[2]);
+            //         // for (int j = 0; j < n_x_; ++j)
+            //         // {
+            //         //     state_traj_opt_.push_back(state_i_opt_);
+            //         // }
         
-       
-        // get the control input
-        ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, 0, "u", &control_opt_);
-       
-        // ROS_INFO("HAVE THE SOLUTION");
+            //     }
 
+
+            // //     // // get the last state
+            // //     // ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, n_nodes_, "x", &state_i_opt_);
+                
+            // // pred_traj_vis();
+
+                
+            // }
+            
+        
+            // get the control input
+            ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, 0, "u", &control_opt_);
+        
+            // ROS_INFO("HAVE THE SOLUTION");
+
+        }
     }
     last_request_time_=current_time;
 }
@@ -229,7 +231,8 @@ void LearningAgile::Update()
     
 
     if (MISSION_LOADED_FLAG_==true)
-    {
+    {   
+        solver_request();
         if (NO_SOLUTION_FLAG_)
         {   
             // traj server will send the current position as the setpoint
@@ -237,29 +240,28 @@ void LearningAgile::Update()
         }
         else
         {
-        solver_request();
-        mavros_msgs::AttitudeTarget mpc_cmd;
-        mpc_cmd.header.stamp = ros::Time::now();
-        mpc_cmd.header.frame_id = origin_frame_;
-        mpc_cmd.type_mask = mavros_msgs::AttitudeTarget::IGNORE_ATTITUDE; // Ignore orientation
-        mpc_cmd.thrust = control_opt_[0]/(single_motor_max_thrust_*4);
-        mpc_cmd.body_rate.x = control_opt_[1];
-        mpc_cmd.body_rate.y = control_opt_[2];
-        mpc_cmd.body_rate.z = control_opt_[3];
-        next_attitude_setpoint_pub_.publish(mpc_cmd);
+            mavros_msgs::AttitudeTarget mpc_cmd;
+            mpc_cmd.header.stamp = ros::Time::now();
+            mpc_cmd.header.frame_id = origin_frame_;
+            mpc_cmd.type_mask = mavros_msgs::AttitudeTarget::IGNORE_ATTITUDE; // Ignore orientation
+            mpc_cmd.thrust = control_opt_[0]/(single_motor_max_thrust_*4);
+            mpc_cmd.body_rate.x = control_opt_[1];
+            mpc_cmd.body_rate.y = control_opt_[2];
+            mpc_cmd.body_rate.z = control_opt_[3];
+            next_attitude_setpoint_pub_.publish(mpc_cmd);
 
-        auto end = std::chrono::high_resolution_clock::now();
+            auto end = std::chrono::high_resolution_clock::now();
 
-        double preloop_dur = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
-        
-        std_msgs::Float64 mpc_runtime;
-        mpc_runtime.data = preloop_dur;
-        mpc_runtime_pub_.publish(mpc_runtime);
+            double preloop_dur = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
+            
+            std_msgs::Float64 mpc_runtime;
+            mpc_runtime.data = preloop_dur;
+            mpc_runtime_pub_.publish(mpc_runtime);
         }  
     }  
     else
     {
-        // ROS_WARN("No mission loaded, will send the hover setpoint");
+        ROS_WARN("No mission loaded, will send the hover setpoint");
         NO_SOLUTION_FLAG_=true;
     }
 }
