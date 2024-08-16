try to repreduce the original training, add 3D plot, fix reward calculation

gestelt_bringup/src/Learning_Agile/deep_learning.py
@@ -18,10 +18,10 @@ subdirectory_path = os.path.join(current_dir, 'Learning_Agile')
 ## deep learning
 # Hyper-parameters 
 
-num_epochs = 1 #100
+num_epochs = 100 #100
 batch_size = 100 # 100
 learning_rate = 1e-4
-num_cores =1 #5
+num_cores =20 #5
 
 # FILE = "nn_pre.pth"
 # model = torch.load(FILE)
@@ -75,7 +75,7 @@ def calc_grad(config_dict,inputs, outputs, gra):
     print("NN1 output traversal pose pitch: ",outputs[4])
     print("NN1 output traversal time: ",outputs[6])
     # receive the decision variables from DNN1, do the MPC, then calculate d_reward/d_z
-    gra[:] = quad1.sol_gradient(outputs[0:3].astype(np.float64),outputs[3:6],np.clip(outputs[6],1.5,3))
+    gra[:] = quad1.sol_gradient(outputs[0:3].astype(np.float64),outputs[3:6],outputs[6])
 
     
 
@@ -103,6 +103,7 @@ if __name__ == '__main__':
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         Iteration = []
         Mean_r = []
+        
         for epoch in range(num_epochs):
         #move = gate1.plane_move()
             evalue = 0
@@ -171,7 +172,9 @@ if __name__ == '__main__':
             mean_reward = evalue/batch_size # evalue/int(batch_size/num_cores)
             Mean_r += [mean_reward]
             print('evaluation: ',mean_reward)
-            np.save(training_data_folder+'/Iteration',Iteration)
-            np.save(training_data_folder+'/Mean_Reward'+str(k),Mean_r)
-            np.save(training_data_folder+'/Every_reward'+str(k),Every_reward)
-        torch.save(model, model_folder+"/NN1_deep2_"+str(k)+".pth")
+            np.save(training_data_folder+'/iteration',Iteration)
+            np.save(training_data_folder+'/mean_reward'+str(k),Mean_r)
+            np.save(training_data_folder+'/every_reward'+str(k),Every_reward)
+
+            if (epoch+1)%10 == 0:
+                torch.save(model, model_folder+"/NN1_deep2_"+str(epoch)+".pth")

gestelt_bringup/src/Learning_Agile/learning_agile_agent.py
@@ -66,12 +66,12 @@ class MovingGate():
     
         ## define the kinematics of the narrow window
         # gate linear velocity
-        # self.v =np.array([0,0.0,0.0]) 
-        self.v =np.array([1,0.3,0.4])
+        self.v =np.array([0,0.0,0.0]) 
+        # self.v =np.array([-1,-0.3,0.4])
         
         # gate pitch angular velocity
-        # self.w = 0 #
-        self.w = pi/2
+        self.w = 0 
+        # self.w = pi/2*(1/3)
         self.gate_move, self.V = self.gate1.move(v = self.v ,w = self.w,dt=dt)
 
         # self.gate_move=np.zeros([1,4,3])
@@ -205,7 +205,7 @@ class LearningAgileAgent():
         self.gate_n = gate(self.gate_move[0])
 
         ##-------------- initial guess of the traversal time---------------##
-        self.t_guess = magni(self.gate_n.centroid-self.state[0:3])/3
+        self.t_guess = magni(self.gate_n.centroid-self.state[0:3])/1
         
     
     def gate_state_estimation(self):
@@ -297,7 +297,7 @@ class LearningAgileAgent():
 
                     # NN2 OUTPUT the traversal time and pose
                     out = self.model(nn_mpc_inputs).data.numpy()
-                    out[6]=out[6]-self.i*self.dyn_step
+                   
                     print('tra_position=',out[0:3],'tra_time_dnn2=',out[6])
 
                 t_comp = time.time()
@@ -349,8 +349,13 @@ class LearningAgileAgent():
         self.quad1.uav1.plot_velocity(self.state_n)
         self.quad1.uav1.plot_quaternions(self.state_n)
         self.quad1.uav1.plot_trav_weight(self.tra_weight_list)
-       
+        self.quad1.uav1.plot_trav_time(self.T)
         self.quad1.uav1.plot_solving_time(self.solving_time)
+        self.quad1.uav1.plot_3D_traj(wing_len=1.5,
+                                    density_list=self.tra_weight_list[::1],
+                                    state_traj=self.state_n[::50,:],
+                                    gate_traj=self.gate_move[::50,:,:])
+
 
         # self.quad1.uav1.plot_T(control_tm)
         # self.quad1.uav1.plot_M(control_tm)

gestelt_bringup/src/Learning_Agile/nn_train_2.py
@@ -8,21 +8,24 @@ import yaml
 # Device configuration
 device = torch.device('cpu')
 #torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# Hyper-parameters 
-input_size = 15 
-hidden_size = 128 
-output_size = 7
-num_epochs = 16*20 #16*100
-batch_size = 20
-num_cores = 1
-learning_rate = 1e-6
 
+conf_folder=os.path.abspath(os.path.join(current_dir, '..', '..','config'))
+yaml_file = os.path.join(conf_folder, 'learning_agile_mission.yaml')
+with open(yaml_file, 'r', encoding='utf-8') as file:
+    config_dict = yaml.safe_load(file)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 training_data_folder=os.path.abspath(os.path.join(current_dir, 'training_data'))
 model_folder=os.path.abspath(os.path.join(training_data_folder, 'NN_model'))
 FILE_INPUT = model_folder+"/NN1_deep2_0.pth"
 model_nn1 = torch.load(FILE_INPUT)
-
+# Hyper-parameters 
+input_size = 15 
+hidden_size = 128 
+output_size = 7
+num_epochs = 16*100 #16*100
+batch_size = config_dict['learning_agile']['horizon']
+num_cores = 20
+learning_rate = 1e-6
 
 model_nn2 = network(input_size, hidden_size, hidden_size,output_size).to(device)
 
@@ -66,10 +69,7 @@ def traj(inputs, outputs, state_traj):
 if __name__ == '__main__':
      # load the configuration file
     # yaml file dir#
-    conf_folder=os.path.abspath(os.path.join(current_dir, '..', '..','config'))
-    yaml_file = os.path.join(conf_folder, 'learning_agile_mission.yaml')
-    with open(yaml_file, 'r', encoding='utf-8') as file:
-        config_dict = yaml.safe_load(file)
+    
     # generate the solver
     quad1 = run_quad(config_dict,  
                     SQP_RTI_OPTION=False, 

gestelt_bringup/src/Learning_Agile/plot_reward.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-reward_data = os.path.join(current_dir, 'Every_Reward0.npy')
-
+reward_file = os.path.join(current_dir, 'training_data/every_reward0.npy')
+reward_data = np.load(reward_file)
 # 绘制奖励图
 plt.figure(figsize=(10, 5))
-plt.plot(reward_data, label='Reward')
+plt.plot(reward_data[59], label='Reward')
 plt.xlabel('Episode')
 plt.ylabel('Reward')
 plt.title('Reward per Episode')

gestelt_bringup/src/Learning_Agile/quad_model.py
@@ -685,8 +685,46 @@ class Quadrotor:
         plt.show()
         
 
+    def plot_trav_time(self,trav_time):
+        plt.figure() 
+        plt.plot(trav_time)
+        plt.title('trav_time vs time')
+        plt.xlabel('t')
+        plt.ylabel('trav_time')
+        plt.grid(True,color='0.6',dashes=(2,2,1,1))
+        plt.legend()
+        plt.savefig('./python_sim_result/trav_time.png')
+        # plt.show()
 
-
+    def plot_3D_traj(self,density_list,wing_len,state_traj,gate_traj):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        position = self.get_quadrotor_position(wing_len, state_traj)
+        for i in range(np.size(gate_traj,0)):
+            p1_x, p1_y, p1_z = gate_traj[i, 0,:]
+            p2_x, p2_y, p2_z = gate_traj[i, 1,:]
+            p3_x, p3_y, p3_z = gate_traj[i, 2,:]
+            p4_x, p4_y, p4_z = gate_traj[i, 3,:]
+            gate_l1, = ax.plot([p1_x,p2_x],[p1_y,p2_y],[p1_z,p2_z],linewidth=1,color='red',linestyle='-',alpha=1/(density_list[i]))
+            gate_l2, = ax.plot([p2_x,p3_x],[p2_y,p3_y],[p2_z,p3_z],linewidth=1,color='red',linestyle='-',alpha=1/(density_list[i]))
+            gate_l3, = ax.plot([p3_x,p4_x],[p3_y,p4_y],[p3_z,p4_z],linewidth=1,color='red',linestyle='-',alpha=1/(density_list[i]))
+            gate_l4, = ax.plot([p4_x,p1_x],[p4_y,p1_y],[p4_z,p1_z],linewidth=1,color='red',linestyle='-',alpha=1/(density_list[i]))
+            c_x, c_y, c_z = position[i,0:3]
+            r1_x, r1_y, r1_z = position[i,3:6]
+            r2_x, r2_y, r2_z = position[i,6:9]
+            r3_x, r3_y, r3_z = position[i,9:12]
+            r4_x, r4_y, r4_z = position[i,12:15]
+            line_arm1, = ax.plot([c_x, r1_x], [c_y, r1_y], [c_z, r1_z], linewidth=1, color='red', marker='o', markersize=1,alpha=1/(density_list[i]))
+            line_arm2, = ax.plot([c_x, r2_x], [c_y, r2_y], [c_z, r2_z], linewidth=1, color='blue', marker='o', markersize=1,alpha=1/(density_list[i]))
+            line_arm3, = ax.plot([c_x, r3_x], [c_y, r3_y], [c_z, r3_z], linewidth=1, color='orange', marker='o', markersize=1,alpha=1/(density_list[i]))
+            line_arm4, = ax.plot([c_x, r4_x], [c_y, r4_y], [c_z, r4_z], linewidth=1, color='green', marker='o', markersize=1,alpha=1/(density_list[i]))
+            # line_arm1.set_data_3d([c_x, r1_x], [c_y, r1_y],[c_z, r1_z])
+            # line_arm2.set_data_3d([c_x, r2_x], [c_y, r2_y],[c_z, r2_z])
+            # line_arm3.set_data_3d([c_x, r3_x], [c_y, r3_y],[c_z, r3_z])
+            # line_arm4.set_data_3d([c_x, r4_x], [c_y, r4_y],[c_z, r4_z])
+        plt.show()    
+    
+    
     def dir_cosine(self, q): # world frame to body frame
         C_B_I = vertcat(
             horzcat(1 - 2 * (q[2] ** 2 + q[3] ** 2), 2 * (q[1] * q[2] + q[0] * q[3]), 2 * (q[1] * q[3] - q[0] * q[2])),

gestelt_bringup/src/Learning_Agile/quad_nn.py
@@ -13,17 +13,29 @@ from quad_model import toQuaternion
 from solid_geometry import norm,magni
 from solid_geometry import plane
 from scipy.spatial.transform import Rotation as R
+import os
+import yaml
+current_dir = os.path.dirname(os.path.abspath(__file__))
+conf_folder=os.path.abspath(os.path.join(current_dir, '..', '..','config'))
+yaml_file = os.path.join(conf_folder, 'learning_agile_mission.yaml')
+with open(yaml_file, 'r', encoding='utf-8') as file:
+    config_dict = yaml.safe_load(file) 
+ini_pos=np.array(config_dict['mission']['initial_position'])
+end_pos=np.array(config_dict['mission']['goal_position'])
+desired_average_vel=config_dict['training_param']['desired_average_vel']
+
+# load the configuration file
 
 ## sample an input for the neural network 1
 def nn_sample(init_pos=None,final_pos=None,init_angle=None):
     inputs = np.zeros(9)
     if init_pos is None:
-        inputs[0:3] = np.random.uniform(-1,1,size=3) + np.array([0,1.8,0]) #-5~5, -9
+        inputs[0:3] = np.random.uniform(-5,5,size=3) + ini_pos #-5~5, -9
     else:
         inputs[0:3] = init_pos
     ## random final postion 
     if final_pos is None:
-        inputs[3:6] = np.random.uniform(-1,1,size=3) + np.array([0,-1.8,0]) #-2~2, 6
+        inputs[3:6] = np.random.uniform(-2,2,size=3) + end_pos #-2~2, 6
     else:
         inputs[3:6] = final_pos
     ## random initial yaw angle of the quadrotor
@@ -53,8 +65,8 @@ def t_output(inputs):
     outputs = np.zeros(7)
     #outputs[5] = math.tan(inputs[6]/2)
     ## traversal time is propotional to the distance of the centroids
-    raw_time=round(magni(inputs[0:3])/2,1)
-    outputs[6] = np.clip(raw_time,1.5,3)
+    raw_time=round(magni(inputs[0:3])/4,1)
+    outputs[6] = np.clip(raw_time,2,4)
     return outputs
 
 ## sample a random gate (not necessary in our method) (not important)

gestelt_bringup/src/Learning_Agile/quad_policy.py
@@ -51,7 +51,7 @@ class run_quad:
         self.uavoc1 = OCSys(config_dict)
         self.uavoc1.setAuxvarVariable()
         sc= 5 #1e20
-        pos_b   = 5 # in each axis
+        pos_b   = config_dict['learning_agile']['pos_bound'] # in each axis
         vel_b   = config_dict['learning_agile']['linear_vel_bound'] #0.5 # in each axis
         wc   = pi/2 #pi
         tw   = 1.22
@@ -83,7 +83,7 @@ class run_quad:
         # wvf: final velocity cost
         # wqf: final attitude cost
         # wwf: final angular velocity cost
-        self.uav1.initCost(wrt=5,wqt=8,wthrust=1,wwt=0,wrf=1,wvf=1,wqf=1) # wthrust = 0.1
+        self.uav1.initCost(wrt=5,wqt=8,wthrust=1,wwt=0,wrf=5,wvf=1,wqf=1) # wthrust = 0.1
         self.uav1.init_TraCost()
         self.uavoc1.setInputCost(self.uav1.input_cost)
         self.uavoc1.setPathCost(self.uav1.goal_cost,goal_state_sym=self.uav1.goal_state_sym)
@@ -146,6 +146,8 @@ class run_quad:
         # state_traj [x,y,z,vx,vy,vz,qw,qx,qy,qz]
         state_traj = self.sol1['state_traj_opt']
 
+
+        # get the quadrotor edges position trajectory
         self.traj = self.uav1.get_quadrotor_position(wing_len = self.winglen, state_traj = state_traj)
         # calculate trajectory reward
         self.collision = 0
@@ -155,8 +157,13 @@ class run_quad:
         for c in range(4):
             self.collision += self.obstacle1.collis_det(self.traj[:,3*(c+1):3*(c+2)],self.horizon)
             self.co += self.obstacle1.co 
+
+        ## calculate the path cost
+        # check the drone centroid position error with the goal position
         for p in range(4):
             self.path += np.dot(self.traj[self.horizon-1-p,0:3]-self.goal_pos, self.traj[self.horizon-1-p,0:3]-self.goal_pos)
+        
+        # the sign of the collision is already negative
         reward = 1000 * self.collision - 0.5 * self.path + 100
         return reward
     # --------------------------- solution and learning----------------------------------------

gestelt_bringup/src/Learning_Agile/solid_geometry.py
@@ -102,12 +102,20 @@ class obstacle():
         self.line4 = line(point4, point1)
 
     def collis_det(self, vert_traj, horizon):
+        """
+        detect collision for each corner of the quadrotor
+        """
         ## define the state whether find corresponding plane
         collision = 0
         self.co = 0
 
         ## judge if the trajectory traverse through the plane
-        if((np.dot(self.plane1.nor_vec(),vert_traj[0]-self.centroid)<0)):
+        PASS_GATE_PLANE = False
+        for i in range(horizon):
+            if((np.dot(self.plane1.nor_vec(),vert_traj[i]-self.centroid)<0)):
+                PASS_GATE_PLANE = True
+                break
+        if not PASS_GATE_PLANE:
             return 0
 
         ## judge whether the first plane is the traversal plane
@@ -115,10 +123,17 @@ class obstacle():
         d_min = 0.2
         for t in range(horizon):
             if(np.dot(self.plane1.nor_vec(),vert_traj[t]-self.centroid)<0):
+
+                # intersect is s_iw
                 intersect = self.plane1.interpoint(vert_traj[t],vert_traj[t-1])
+                
+                
                 # judge whether they belong to plane1 and calculate the distance
                 if(np.dot(self.plane1.n1(),intersect-self.centroid)>0 and np.dot(self.plane1.n2(),intersect-self.centroid)>0):
+                    
                     if(np.dot(self.point1-intersect,self.plane1.n3())>0):
+
+                        # m is the d_i
                         m = min(self.line1.vertical(intersect),self.line2.vertical(intersect),\
                             self.line3.vertical(intersect),self.line4.vertical(intersect))
                         collision = - max(0,d_min-m)**2