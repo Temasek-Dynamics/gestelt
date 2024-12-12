2ad7ed543f7fad65dc532ede070f44d3e3dbf13f
tianchensun <tianchen_sun787@163.com>
2024-12-12 14:23:44 +0800

    update self ip in the shell

--- gestelt_bringup/scripts/sitl_bringup_learning_agile_drone_distributed.sh
+++ gestelt_bringup/scripts/sitl_bringup_learning_agile_drone_distributed.sh
@@ -19,7 +19,7 @@ source $SCRIPT_DIR/../../../devel/setup.bash &&
 # export ROS_MASTER_URI (for distributed simulation)
 # drone's side ROS_MASTER_URI should be the laptop
 EXPORT_ROS_MASTER_URI="
-export ROS_HOSTNAME=172.20.10.2 &&
+export ROS_HOSTNAME=$SELF_IP &&
 export ROS_MASTER_URI=http://172.20.10.3:11311
 "
 
