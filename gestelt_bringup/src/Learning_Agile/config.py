33660a972f58a946b89cf40f079a59683a505fc4
tianchensun <tianchen_sun787@163.com>
2024-12-12 12:49:45 +0800

    update config

--- gestelt_bringup/src/Learning_Agile/config.py
+++ gestelt_bringup/src/Learning_Agile/config.py
@@ -2,7 +2,7 @@ import os
 import datetime
 import logging
 import yaml
-from torch.utils.tensorboard import SummaryWriter
+
 
 from logger_misc import LoggerConfig,log_gradient,log_train_IO
 
@@ -52,6 +52,7 @@ with open(training_yaml_file, 'r', encoding='utf-8') as file:
 # 延迟执行的目录创建逻辑
 def setup_training_directories(base_dir='training_results'):
     """仅在需要时创建训练目录和子目录"""
+   
     training_results_folder = os.path.abspath(os.path.join(current_dir, base_dir))
     if not os.path.exists(training_results_folder):
         os.makedirs(training_results_folder)
