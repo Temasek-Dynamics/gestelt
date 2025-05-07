5a88771d8b37fa3e7aa0edcc67119f9dc4d06947
tianchensun <tianchen_sun787@163.com>
2025-05-07 16:01:05 +0800

    bug fix

--- mpc_ros_wrapper/include/mpc_ros_wrapper/mpc_ros_wrapper.h
+++ mpc_ros_wrapper/include/mpc_ros_wrapper/mpc_ros_wrapper.h
@@ -53,7 +53,7 @@ class mpcRosWrapper{
         void drone_state_twist_cb(const geometry_msgs::TwistStamped::ConstPtr& msg);
         void drone_state_body_rate_cb(const geometry_msgs::TwistStamped::ConstPtr& msg);
         void NN_trav_pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg);
-        void close_loop_NN_trav_pose_cb(const gestelt_msgs::close_loop_NN_output::ConstPtr& msg);
+        void close_loop_NN_output_cb(const gestelt_msgs::close_loop_NN_output::ConstPtr& msg);
         void NN_trav_time_cb(const std_msgs::Float32::ConstPtr& msg);
 
         //------------------acados solver-------------------
