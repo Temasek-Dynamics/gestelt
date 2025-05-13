0ad8bc03b80b163e49d8b635e2c0dead5f73acb0
tianchensun <tianchen_sun787@163.com>
2025-05-13 10:01:39 +0800

    script refine

--- gestelt_bringup/scripts/real_flight_learning_agile_remote.sh
+++ gestelt_bringup/scripts/real_flight_learning_agile_remote.sh
@@ -32,12 +32,13 @@ export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$gestelt_bringup_DIR:$PX4_AUTOPILOT_RE
 # Commands
 #####
 
-CMD_3="
-roslaunch gestelt_bringup NN_ROS_wrapper.launch
-"
 
 CMD_2="
 roslaunch gestelt_bringup rviz.launch rviz_config:= rviz_config:=gz_real 
+"
+
+CMD_3="
+roslaunch gestelt_bringup NN_ROS_wrapper.launch
 "
 
 CMD_4="
@@ -60,7 +61,7 @@ then
     sleep 1
     tmux send-keys -t $SESSION:0.1 "$SOURCE_WS $EXPORT_ROS_MASTER_URI $CMD_2" C-m 
     sleep 1
-    tmux send-keys -t $SESSION:0.2 "$SOURCE_WS $EXPORT_ROS_MASTER_URI " C-m 
+    tmux send-keys -t $SESSION:0.2 "$SOURCE_WS $EXPORT_ROS_MASTER_URI $CMD_3" C-m 
     sleep 1
     tmux send-keys -t $SESSION:0.3 "$SOURCE_WS $EXPORT_ROS_MASTER_URI $CMD_4" C-m 
 fi
