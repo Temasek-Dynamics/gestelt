6b8c6e65d57aaa0cd7ccdda8b8753bd54040f3b3
tianchensun <tianchen_sun787@163.com>
2025-06-12 10:31:37 +0800

    update IP

--- gestelt_bringup/scripts/real_flight_learning_agile_remote.sh
+++ gestelt_bringup/scripts/real_flight_learning_agile_remote.sh
@@ -19,9 +19,9 @@ source $SCRIPT_DIR/../../../devel/setup.bash &&
 # export ROS_MASTER_URI (for distributed simulation)
 # drone's side ROS_MASTER_URI should be the laptop
 EXPORT_ROS_MASTER_URI="
-export ROS_IP=192.168.31.204 && 
-export ROS_HOSTNAME=192.168.31.204 &&
-export ROS_MASTER_URI=http://192.168.31.205:11311
+export ROS_IP=192.168.1.13 && 
+export ROS_HOSTNAME=192.168.1.13 &&
+export ROS_MASTER_URI=http://192.168.1.12:11311
 "
 # PX4 v1.13.0
 SOURCE_PX4_AUTOPILOT="
